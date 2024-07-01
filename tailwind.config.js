dbf9bad34c71838f39112eeade13248685ff264b
kahon <kahonbintezaman@gmail.com>
2024-07-01 18:44:27 +0600

    signup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        lavender: '#E6E6FA',
+        violet: {
+          500: '#7F00FF', // Customized violet color
+        },
+      },
+    },
+  },
+  plugins: [],
+};
